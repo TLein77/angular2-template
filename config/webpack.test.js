6355508cbddffed0fe1671345d849cfcd9065186
Todd <tleininger@gmail.com>
2016-12-15 11:11:45 -0600

    Code for a basic angular 2 app using webpack

---
+++ config/webpack.test.js
@@ -0,0 +1,49 @@
+var helpers = require('./helpers');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    devtool: 'inline-source-map',
+
+    resolve: {
+        extensions: ['', '.ts', '.js']
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.ts$/,
+                loaders: ['awesome-typescript-loader', 'angular2-template-loader']
+            },
+            {
+                test: /\.html$/,
+                loader: 'html'
+            },
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url-loader?limit=10000&minetype=application/font-woff'
+            },
+            {
+                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'file-loader'
+            },
+            {
+                test: /\.(png|jpe?g|gif|ico)$/,
+                loader: 'file?name=assets/[name].[hash].[ext]'
+            },
+            {
+                test: /\.pdf$/,
+                loader: 'file-loader?name=[name].[ext]&publicPath=assets/'
+            },
+            {
+                test: /\.css$/,
+                exclude: helpers.root('src', 'app'),
+                loader: ExtractTextPlugin.extract('style', 'css?sourceMap')
+            },
+            {
+                test: /\.css$/,
+                include: helpers.root('src', 'app'),
+                loader: 'raw'
+            }
+        ]
+    }
+};
---
+++ src/vendor.ts
@@ -0,0 +1,11 @@
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/core';
+import '@angular/common';
+import '@angular/http';
+import '@angular/router';
+import 'rxjs';
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/jquery/dist/jquery.min.js';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
