0577f4b480828b4fb51c891c68b12b3e710f11e2
Todd <tleininger@gmail.com>
2016-12-15 11:23:24 -0600

    A little more to the default app.spec

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,7 +3,11 @@ import {AppComponent} from "./app.component";
 
 describe('App', () => {
     beforeEach(() => {
-        TestBed.configureTestingModule({declarations: [AppComponent]});
+        TestBed.configureTestingModule({
+            imports: [],
+            declarations: [AppComponent],
+            providers: []
+        });
     });
     it ('should work', () => {
         let fixture = TestBed.createComponent(AppComponent);
